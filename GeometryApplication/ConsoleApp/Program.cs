0c9a8eb64d1a9a1e2b3f75dc6530a85c10c47654
anookala312 <156244126+anookala312@users.noreply.github.com>
2024-04-17 01:45:00 +0000

    featuremanagement

--- GeometryApplication/ConsoleApp/Program.cs
+++ GeometryApplication/ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Configuration;
+using Microsoft.Extensions.Configuration;
+var featureManagement = new Dictionary<string, string> {{ "FeatureManagement:Square", "true"}, { "FeatureManagement:Rectangle", "false"}, { "FeatureManagement:Triangle", "true"}};
+
+IConfigurationRoot configuration = new ConfigurationBuilder().AddInMemoryCollection(featureManagement).Build();
+
+var services = new ServiceCollection();
+services.AddFeatureManagement(configuration);
+var serviceProvider = services.BuildServiceProvider();
+
+var featureManager = serviceProvider.GetRequiredService<IFeatureManagerSnapshot>();
+if (await featureManager.IsEnabledAsync("Square"))
+{
+    
+}

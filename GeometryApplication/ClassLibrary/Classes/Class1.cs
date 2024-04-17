9a5ffcbf2650e2dadbf7c811ccd54375b361c90d
anookala312 <156244126+anookala312@users.noreply.github.com>
2024-04-17 00:03:54 +0000

    create library, console and test projects

---
+++ GeometryApplication/ClassLibrary/Classes/Class1.cs
@@ -0,0 +1,41 @@
+﻿namespace ClassLibrary.Classes;
+
+public interface IShape
+{
+    double CalculateArea();
+    double CalculatePerimeter();
+}
+public class Square : IShape
+{
+    public static double CalculateArea(length)
+    {
+        return length*length;
+    }
+    public static double CalculatePerimeter()
+    {
+        return 4*length
+    }
+}
+public class Rectangle : IShape
+{
+    public static double CalculateArea(length, width)
+    {
+        return length*width;
+    }
+    public static double CalculatePerimeter(length, width)
+    {
+        return 2*(length+width)
+    }
+}
+
+public class Rectangle : IShape
+{
+    public static double CalculateArea(width, height)
+    {
+        return (width*height)/2;
+    }
+    public static double CalculatePerimeter(length, width)
+    {
+        return 2*(length+width)
+    }
+}
